323a51495ff0b9b4a3d6040b493066fa31f79220
Alberto Restifo <alberto@restifo.dev>
2021-12-04 22:09:40 +0100

    Complete solution for part 2

--- 03/03.rkt
+++ 03/03.rkt
@@ -25,16 +25,17 @@
 (define (calc-gamma input)
   (let ([half (/ (length input) 2)])
     (for/list ([sum (sum-cols input)])
-      (if (> sum half)
+      (if (>= sum half)
           1
           0))))
 
+;; Flipts the value of a bit
+(define (flip bit)
+  (if (= bit 1) 0 1))
+
 ;; Inverts the sequence
 (define (invert sequence)
-  (for/list ([v sequence])
-    (if (= v 1)
-        0
-        1)))
+  (map flip sequence))
 
 ;; Converts a list containing bytes initigers into a number
 (define (blist->number blist)
@@ -52,3 +53,35 @@
 ;; Print the part 1 solutions
 (printf "[TEST] Part 1: ~a\n" (part-1 "03/test"))
 (printf "[REAL] Part 1: ~a\n" (part-1 "03/input"))
+
+;; Return true when this bit should be kept according to the flag
+(define (should-keep? bit flag flip?)
+  (if (eq? bit flag)
+      (if flip? false true)
+      (if flip? true false)))
+
+;; Filter list matching the flag at the given position
+(define (filter-flag-at input flags pos flip?)
+  (filter (lambda (values)
+            (should-keep? (list-ref values pos) (list-ref flags pos) flip?))
+          input))
+
+;; Keep filtering the list, moving the position at each iteration until
+;; we're left with only one element in the list
+(define (find-match input flags flip? [pos 0])
+  (let ([filtered (filter-flag-at input flags pos flip?)])
+    (if (equal? 1 (length filtered))
+        (flatten filtered)
+        (find-match filtered (calc-gamma filtered) flip? (add1 pos)))))
+
+;; Obtain the answer to part 2
+(define (part-2 file-path)
+  (let* ([input (parse-input file-path)]
+         [gamma (calc-gamma input)]
+         [oxigen (find-match input gamma false)]
+         [co2 (find-match input gamma true)])
+    (* (blist->number oxigen) (blist->number co2))))
+
+;; Print the part 2 solutions
+(printf "[TEST] Part 2: ~a\n" (part-2 "03/test"))
+(printf "[REAL] Part 2: ~a\n" (part-2 "03/input"))
